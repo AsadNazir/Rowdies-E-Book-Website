8ce3905315d624669b790dc853b5392a6e834a66
AsadNazir <79693635+AsadNazir@users.noreply.github.com>
2021-12-15 19:01:56 +0500

    Added books section Fixed Mobile Version issues

--- Index.js
+++ Index.js
@@ -2,20 +2,24 @@ const Menu_open = document.querySelector(`#Open`);
 const Menu_Close = document.querySelector(`#Close`);
 const Menu = document.querySelector(`.side_menu`);
 const Website = document.querySelector(`.website`);
-// let isMenuOpen = false;
+const body = document.querySelector(`#all`)
+const ul = document.querySelector(`#side_menu_ul`)
+    // let isMenuOpen = false;
 
 //Adding event listeners 
 
 //Openinig Event Listner 
 Menu_open.addEventListener(`click`, function() {
-    Menu.style.transform = `translateX(0%)`;
+
+    Menu.style.opacity = `1`;
+    Menu.style.transform = `translateX(0%) scaleX(1)`;
+
+    // Website.style.opacity = `0.6`;
+    // body.style.height = `100vh`;
+    // body.style.overflow = `hidden`;
 
 
-    Website.style.opacity = `0.6`;
-    Website.style.height = `99vh`;
-    Website.style.overflow = `hidden`;
     console.log("here");
-
 })
 
 
@@ -25,15 +29,19 @@ Menu_Close.addEventListener(`click`, Close_func)
 
 //Close Fucntion
 function Close_func() {
-    Menu.style.transform = `translateX(100%)`;
+    Menu.style.opacity = `0`
+    Menu.style.transform = `translateX(100%) scaleX(0)`;
 
-    isMenuOpen = false;
 
-    Website.style.opacity = `1`;
-    Website.style.height = `inherit`;
-    Website.style.overflow = `visible`;
+    // Website.style.opacity = `1`;
+    // body.style.height = `inherit`;
+    //body.style.overflow = `visible`;
+
+
+
 
 }
+
 
 
 // {margin: 0; height: 100%; overflow: hidden}
